9aa8ae2af1e4cb00041c86d150c95210b5e55640
sanskritinegi2401 <sanskriti.negi@ubs.com>
2024-02-29 15:12:29 +0530

    Added frontend & backend code

---
+++ frontend/src/containers/Landing.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Landing = () =>{
+    
+    return(
+        <>
+        <div className="landing-page">
+                <div className="buttons">
+                    <button className="explore-button">Explore</button>
+                    <button className="add-property-button">Add Property</button>
+                </div>
+         </div>
+        </>
+        
+
+    );
+
+};
+
+export default Landing;
